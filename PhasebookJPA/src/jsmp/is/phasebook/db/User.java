fe18f44b893964aa8243957d7242fb4d41e417b1
Jose Santos <jsmpereira@gmail.com>
2011-10-20 19:21:42 +0100

    Initial commit of Phasebook JPA module

---
+++ PhasebookJPA/src/jsmp/is/phasebook/db/User.java
@@ -0,0 +1,79 @@
+package jsmp.is.phasebook.db;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users") 
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private boolean loggedIn;
+	
+	public User() {}
+	
+	public User(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+}
